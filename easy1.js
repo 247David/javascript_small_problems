06d5a49849ec1bba461f99257219883f9a897db9
David Jaiyeola <davidjaiye247@gmail.com>
2023-08-08 01:37:47 -0400

    Started easy1 exercises.

---
+++ easy1.js
@@ -0,0 +1,25 @@
+//1 . Isn't it Odd? -  Write a function that takes one integer argument, which may be positive, negative, or 
+//zero. This method returns true if the number's absolute value is odd. You may assume that the argument is a 
+//valid integer value.
+
+
+// let isOdd = function (inputInt) {
+//     return Math.abs(inputInt) % 2 === 1;
+// };
+
+// console.log(isOdd(2)); // => false
+// console.log(isOdd(5)); // => true
+// console.log(isOdd(-17)); // => true
+// console.log(isOdd(-8)); // => false
+// console.log(isOdd(0)); // => false
+// console.log(isOdd(7)); // => true
+
+//2. Odd Numbers - Log all odd numbers from 1 to 99, inclusive, to the console, with each number 
+//on a separate line.
+let n  = 0;
+while (n < 100) {
+    if (n % 2 === 1) {
+        console.log(n);
+    }
+    n +=1;
+}
