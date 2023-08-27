c7ecf11c6e6417749c739dee98126f74b57d3020
David Jaiyeola <davidjaiye247@gmail.com>
2023-08-26 22:57:03 -0400

    Added to easy1.js exercise file.

--- easy1.js
+++ easy1.js
@@ -27,13 +27,13 @@
 //3. Even Numbers - Log all even numbers from 1 to 99, inclusive, to the console, with each number 
 //on a separate line.
 
-let numbers = 1;
+// let numbers = 1;
 
-while (numbers < 100) {
-    if (numbers % 2 === 0){
-        console.log(numbers);
-    }numbers += 1;
-}
+// while (numbers < 100) {
+//     if (numbers % 2 === 0){
+//         console.log(numbers);
+//     }numbers += 1;
+// }
 
 //4. How big is the room? - Build a program that asks the user to enter the length and width of a room in 
 //meters, and then logs the area of the room to the console in both square meters and square feet.
@@ -41,3 +41,25 @@ while (numbers < 100) {
 // Do not worry about validating the input at this time. Use the readlineSync.prompt method to collect 
 //user input.
 
+// const input = require('readline-sync');
+
+// let length = input.question('Room length in meters: ');
+// let width = input.question('Room width in meters: ');
+
+// console.log(((Number(length) * 10.7639)) + ((Number(width)) * 10.7639) + 'square feet');
+
+
+//5. Tip Calculator - Create a simple tip calculator. The program should prompt for a bill 
+// amount and a tip rate. The program must compute the tip, and then log both the tip and 
+// the total amount of the bill to the console. You can ignore input validation and assume
+// that the user will enter numbers.
+
+const input = require('readline-sync');
+
+let billAmount = Number(input.question('Enter bill amount:'));
+
+let tipRate = Number(input.question('Enter tip rate(%):'));
+
+let totalAmount = billAmount + (billAmount * (tipRate/100));
+
+console.log(`Tip is: ${billAmount * (tipRate/100)} and the total bill is: ${totalAmount} `);
