ac36c46f5799135b9170a6f6713d301e650b74bc
David Jaiyeola <davidjaiye247@gmail.com>
2023-09-03 21:01:14 -0400

    Completed easy 1 exercises.

--- easy1.js
+++ easy1.js
@@ -114,6 +114,129 @@ let shortLongShort = function (inputString1,inputString2) {
 //divisible by 100, then it is not a leap year, unless the year is also evenly divisible by 400.
 //Assume this rule is valid for any year greater than year 0. Write a function that takes any year greater 
 //than 0 as input and returns true if the year is a leap year, or false if it is not a leap year.
-// isLeapYear(2016);      // true
+// summary:
+// - year that is evenly divisible by 4
+
+// - If the year is evenly divisible by 100
+// - unless the year is also evenly divisible by 400
+// - true
+// - false
+// - 
+// -true
+
+// else 
+// - false
+
+//  function isLeapYear (year) {
+//     if(year % 4 === 0) {
+//         if(year % 100 === 0){
+//             if(year % 400 === 0){
+//                 return true;
+//             }
+//             return false;
+//         }
+//         return true;
+//     }else{
+//         return false;
+//     }
+//  }
+
+//  console.log(isLeapYear(2016));      // true
+//  console.log(isLeapYear(2015));      // false
+//  console.log(isLeapYear(2100));      // false
+//  console.log(isLeapYear(2400));      // true
+//  console.log(isLeapYear(240000));    // true
+//  console.log(isLeapYear(240001));    // false
+//  console.log(isLeapYear(2000));      // true
+//  console.log(isLeapYear(1900));      // false
+//  console.log(isLeapYear(1752));      // true
+//  console.log(isLeapYear(1700));      // false
+//  console.log(isLeapYear(1));         // false
+//  console.log(isLeapYear(100));       // false
+//  console.log(isLeapYear(400));       // true
+
+//9. Leap Years (Part 2) - This is a continuation of the previous exercise. The British Empire adopted 
+//the Gregorian Calendar in 1752, which was a leap year. Prior to 1752, they used the Julian Calendar. 
+//Under the Julian Calendar, leap years occur in any year that is evenly divisible by 4.
+//Using this information, update the function from the previous exercise to determine leap years both 
+//before and after 1752.
+
+//  function isLeapYear (year) {
+
+//     if(year < 1752  ){
+//         if(year % 4 === 0){
+//             return true;
+//         }else{
+//             return false;
+//         }
+//     }
+//     else if(year % 4 === 0) {
+//         if(year % 100 === 0){
+//             if(year % 400 === 0){
+//                 return true;
+//             }
+//             return false;
+//         }
+//         return true;
+//     }else{
+//         return false;
+//     }
+//  }
+//  console.log(isLeapYear(2016));      // true
+//  console.log(isLeapYear(2015));      // false
+//  console.log(isLeapYear(2100));      // false
+//  console.log(isLeapYear(2400));      // true
+//  console.log(isLeapYear(240000));    // true
+//  console.log(isLeapYear(240001));    // false
+//  console.log(isLeapYear(2000));      // true
+//  console.log(isLeapYear(1900));      // false
+//  console.log(isLeapYear(1752));      // true
+//  console.log(isLeapYear(1700));      // false
+//  console.log(isLeapYear(1));         // false
+//  console.log(isLeapYear(100));       // false
+//  console.log(isLeapYear(400));       // true
+
+//10. Multiples of 3 and 5 - Write a function that computes the sum of all numbers between 1 and 
+// some other number, inclusive, that are multiples of 3 or 5. For instance, if the supplied number is 20, 
+//the result should be 98 (3 + 5 + 6 + 9 + 10 + 12 + 15 + 18 + 20). You may assume that the number passed in 
+//is an integer greater than 1.
+
+// multisum(3);       // 3
+// multisum(5);       // 8
+// multisum(10);      // 33
+// multisum(1000);    // 234168
 
 
+// function multisum(numb){
+//     let total = 0;
+//     while (numb > 1){
+//         if(numb % 3 === 0 || numb % 5 === 0){
+//             total += numb;
+//         }
+//         numb -=1;
+//     }
+//     return total;
+// }
+
+// console.log(multisum(3));       // 3
+// console.log(multisum(5));       // 8
+// console.log(multisum(10));      // 33
+// console.log(multisum(1000));    // 234168
+
+//11. UTF-16 String Value - Write a function that determines and returns the UTF-16 string value of a string 
+// passed in as an argument. The UTF-16 string value is the sum of the UTF-16 values of every character in the 
+// string. (You may use String.prototype.charCodeAt() to determine the UTF-16 value of a character.)
+// utf16Value('Four score');         // 984
+
+function utf16Value(input){
+    utfSum = 0;    
+    for(let char in input){
+        utfSum += input[char].charCodeAt();    
+    }
+    return utfSum;
+}
+
+console.log(utf16Value('Four score'));
+console.log(utf16Value('Launch School'));
+console.log(utf16Value('a'));
+console.log(utf16Value('\u03A9'));
