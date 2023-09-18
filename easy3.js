488cf5967fb286d4466a3918ee27f488d0a43f67
David Jaiyeola <davidjaiye247@gmail.com>
2023-09-17 23:59:18 -0400

    Added exercises to easy3.js.

--- easy3.js
+++ easy3.js
@@ -77,3 +77,26 @@
 // };
 
 // console.log(stringy(9));
+
+//4. Right Triangles - Write a function that takes a positive integer, n, as an argument and logs a right 
+//triangle whose sides each have n stars. The hypotenuse of the triangle (the diagonal side in the images below)
+// should have one end at the lower-left of the triangle, and the other end at the upper-right.
+// Examples:
+// triangle(5);
+
+//     *
+//    **
+//   ***
+//  ****
+// *****
+
+let triangle = function(triangleSize){
+    for(let i = 0; i < triangleSize; i++) {
+        for (let j = 0; j<= i; j++){
+            process.stdout.write('*');
+        }
+        process.stdout.write('\n');
+    }
+};
+
+triangle(4);
